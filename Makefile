88f6de7e95cf44d1de1c25e4b0f0d517f64c26af
tomasagata <tomasagata00@gmail.com>
2025-01-22 21:55:48 +0000

    Removed old makefile, cleaned up new start.sh script

--- Makefile
+++
@@ -1,8 +0,0 @@
-test:
-	cd src && sudo python network.py ../data/test.pcap
-
-real:
-	cd src && sudo python network.py ../data/dataset.pcap	
-
-clean:
-	rm -rf src/log src/pcap src/topology.json src/p4/*.json src/p4/*.p4i
--- start.sh
+++ start.sh
@@ -1,12 +1,67 @@
 #!/usr/bin/env bash
 
-if [ $# -lt 3 ]; then
-  echo "Usage: ${$0} PCAP_FILE THRESHOLD";
-  exit 1;
+__usage="
+Usage: $0 [-t pkts | -k flows] -p file
+
+Options:
+  -p, --pcap <file>            Specify the packet capture file for the test.
+  -t, --threshold <packets>    Specify the packet threshold to start considering as a heavy hitter.
+  -k, --top-k <flows>          Specify the amount of flows to consider as heavy hitters.
+  -h, --help                   Print this usage and exit.
+
+"
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -p|--pcap-file)
+      PCAP_FILE="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -t|--threshold)
+      THRESHOLD="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -k|--top-k)
+      TOP_K="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -h|--help)
+      HELP=YES
+      shift # past argument
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      echo "Unknown argument $1"
+      exit 1
+      ;;
+  esac
+done
+
+if [ -n "$HELP" ]; then
+  echo "$__usage"
+  exit 0
 fi
 
-PCAP_FILE=$1
-THRESHOLD=$2
+if [ -z "$PCAP_FILE" ]; then
+  echo "ERROR: Missing packet capture file."
+  exit 1
+fi
+
+if [ -z "$TOP_K" ] && [ -z "$THRESHOLD" ]; then
+  echo "ERROR: Missing --top-k or --threshold flag."
+  exit 1
+fi
+
+if [ -n "$TOP_K" ] && [ -n "$THRESHOLD" ]; then
+  echo "ERROR: --top-k and --threshold flags are mutually exclusive."
+  exit 1
+fi
 
 echo "Generating ground truth..."
 python3 src/scripts/gtruth.py $PCAP_FILE $THRESHOLD
